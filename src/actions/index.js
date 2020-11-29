cf2686340ac2c88f955fc760d623b32fd5191999
Minetoshi <minetoshi.api@gmail.com>
2020-11-30 07:46:00 +0900

    commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 export const CREATE_EVENT = 'CREATE_EVENT'
 export const UPDATE_EVENT = 'UPDATE_EVENT'
 
-export const putEvent = values => async dispatch => {
-  const response = await axios.put(`http://localhost:8080/estimate/medical/getfee`, values)
-  dispatch({ type: UPDATE_EVENT, response })
-}
+// export const putEvent = values => async dispatch => {
+//   const response = await axios.put(`http://localhost:8080/estimate/medical/getfee`, values)
+//   dispatch({ type: UPDATE_EVENT, response })
+// }
 export const postEvent = values => async dispatch => {
-  const response = await axios.post(`http://localhost:8080/estimate/medical/getfee}`, values)
+  const response = await axios.post(`http://nrt.ocir.io/nrw7mnqnx9dw/anonymous/backend:latest}`, values)
   dispatch({ type: CREATE_EVENT, response })
 }
 
