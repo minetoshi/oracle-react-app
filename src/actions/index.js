82b46c00c81c8a93bbea1d907660b1a5ff2b9450
Minetoshi <minetoshi.api@gmail.com>
2020-11-30 18:07:40 +0900

    urlの変更

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,11 +4,11 @@ export const CREATE_EVENT = 'CREATE_EVENT'
 export const UPDATE_EVENT = 'UPDATE_EVENT'
 
 // export const putEvent = values => async dispatch => {
-//   const response = await axios.put(`http://localhost:8080/estimate/medical/getfee`, values)
+//   const response = await axios.put(`http://158.101.74.170`, values)
 //   dispatch({ type: UPDATE_EVENT, response })
 // }
 export const postEvent = values => async dispatch => {
-  const response = await axios.post(`http://nrt.ocir.io/nrw7mnqnx9dw/anonymous/backend:latest}`, values)
+  const response = await axios.post(`http://158.101.74.170`, values)
   dispatch({ type: CREATE_EVENT, response })
 }
 
